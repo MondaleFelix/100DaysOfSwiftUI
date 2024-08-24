a15a93d42061a61c1ad62d10e6c89d03b61dbde8
MondaleFelix <mondalefelix@yahoo.com>
2024-08-23 23:34:14 -0700

    Day 1 Completed

---
+++ Day1/Day1.playground/Contents.swift
@@ -0,0 +1,96 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// Variable
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+// Constant
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+let actor = "Denzel Washington"
+
+let fileName = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+
+print(fileName.hasSuffix(".jpg"))
+
+let score = 10
+
+let reallyBig = 1_____00_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter = counter + 5
+
+counter += 5
+print(counter)
+
+counter *= 2
+print(counter)
+
+counter -= 10
+print(counter)
+
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+print(120.isMultiple(of: 3))
+
+let decimalNumber = 0.1 + 0.2
+print(decimalNumber)
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var name1 = "Nicolas Cage"
+name1 = "John Travolta"
+
+var rating = 5.0
+rating *= 2
